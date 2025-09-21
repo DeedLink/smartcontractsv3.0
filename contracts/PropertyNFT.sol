fa6f0dc03150b89b9661fdd051c9cc466996b1c6
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-09-21 07:23:25 +0530

    update

--- contracts/PropertyNFT.sol
+++ contracts/PropertyNFT.sol
@@ -3,16 +3,14 @@ pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract PropertyNFT is ERC721, Ownable {
     uint256 public nextTokenId;
     mapping(uint256 => string) public tokenURIMap;
-    bytes32 public merkleRoot;
 
     constructor(address initialOwner) ERC721("RealEstateNFT", "RE-NFT") Ownable(initialOwner){}
 
-    function mintProperty(address to, string memory uri) external {
+    function mintProperty(address to, string memory uri) external onlyOwner {
         uint256 tokenId = nextTokenId++;
         _safeMint(to, tokenId);
         tokenURIMap[tokenId] = uri;
