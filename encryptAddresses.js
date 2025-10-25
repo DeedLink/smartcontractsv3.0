43990bb144578b35bd1efca2b2bca97cec1a02c6
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-25 19:04:06 +0530

    update

---
+++ encryptAddresses.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+const algorithm = 'aes-256-cbc';
+const key = process.env.ENCRYPTION_KEY;
+const iv = crypto.randomBytes(16);
+
+const data = fs.readFileSync('path/to/contract-addresses.json', 'utf8');
+
+const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, 'hex'), iv);
+let encrypted = cipher.update(data, 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+const encryptedData = {
+  iv: iv.toString('hex'),
+  data: encrypted
+};
+
+fs.writeFileSync('path/to/encrypted-addresses.json', JSON.stringify(encryptedData));
+
+console.log('Encryption complete');
